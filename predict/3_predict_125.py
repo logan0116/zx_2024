1c9c3df24346181a2a2e57f916eeaf23d71484d7
logan0116 <1809361150@qq.com>
2024-08-01 08:52:38 +0800

    update: predict

--- predict/3_predict_125.py
+++ predict/3_predict_125.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 """
 @Project ：zx_2 
-@File    ：3_predict.py
+@File    ：3_predict_125.py
 @IDE     ：PyCharm 
 @Author  ：Logan
 @Date    ：2023/12/9 上午12:42 
@@ -19,21 +19,19 @@ from tqdm import tqdm
 def get_inputs(year):
     device = 'cuda:0'
     node2predict_c = {}
-    category_list = ['高端装备制造', '节能环保', '生物', '数字创意', '新材料', '新能源', '新能源汽车', '新一代信息技术']
-
-    c2threshold = {0: 0.9,
-                   1: 0.5,
-                   2: 0.995,
-                   3: 0.5,
-                   4: 0.99,
-                   5: 0.5,
-                   6: 0.5}
+    c2threshold = {
+        0: 0.5,
+        1: 0.5,
+        2: 0.5,
+        3: 0.5,
+        4: 0.5,
+        5: 0.5,
+        6: 0.5,
+        7: 0.5
+    }
 
     for category in tqdm(range(8), desc=str(year)):
-        if category in [0, 1, 2, 4, 5]:
-            node2vec_path = 'data/node2vec_{}_v2.json'.format(year)
-        else:
-            node2vec_path = 'data/node2vec_{}_v0.json'.format(year)
+        node2vec_path = 'data/node2vec_{}_v0.json'.format(year)
         # load node2vec by json
         with open(node2vec_path, 'r', encoding='utf-8') as f:
             node2vec = json.load(f)
@@ -46,12 +44,8 @@ def get_inputs(year):
 
         # load model
         model = MyDnnSimple()
-        if category in [0, 1, 2, 4, 5]:
-            model_state_dict = torch.load('model_final_v2/Dnn_{}_v1.pt'.format(category))
-            c = np.load('data/category2vec_v1.npy')
-        else:
-            model_state_dict = torch.load('model_final_v2/Dnn_{}_v0.pt'.format(category))
-            c = np.load('data/category2vec_v0.npy')
+        model_state_dict = torch.load('model/Dnn_125_{}.pt'.format(category))
+        c = np.load('../dnn/data/category2vec_125.npy')
         c = torch.Tensor(c[category])
         c = c.to(device)
 
@@ -64,13 +58,7 @@ def get_inputs(year):
         y_pred = torch.sigmoid(y_pred)
 
         # 根据y0和y1的差异进行预测
-        if category != 7:
-            y_pred = torch.where(y_pred > c2threshold[category], torch.ones_like(y_pred), torch.zeros_like(y_pred))
-        else:
-            # 非常抽象的做法，先这样。
-            threshold_list_7 = [0.999 - i * 0.0001 for i in range(12)]
-            y_pred = torch.where(y_pred > threshold_list_7[year - 2011],
-                                 torch.ones_like(y_pred), torch.zeros_like(y_pred))
+        y_pred = torch.where(y_pred > c2threshold[category], torch.ones_like(y_pred), torch.zeros_like(y_pred))
         # print(y_pred)
         # y_pred = torch.argmax(y_pred, dim=1)
         y_pred = y_pred.cpu().numpy().tolist()
@@ -79,20 +67,20 @@ def get_inputs(year):
         node2predict_c[category] = node2pred
 
     # save by json
-    with open('data/result/node2predict_c_{}.json'.format(year), 'w', encoding='utf-8') as f:
+    with open('result/node2predict_c_{}.json'.format(year), 'w', encoding='utf-8') as f:
         json.dump(node2predict_c, f, ensure_ascii=False, indent=4)
 
 
 def analysis():
     # load node2predict_c
     node2predict_c = {}
-    for year in range(2011, 2023):
-        with open('data/result/node2predict_c_{}.json'.format(year), 'r', encoding='utf-8') as f:
+    for year in range(2011, 2016):
+        with open('result/node2predict_c_{}.json'.format(year), 'r', encoding='utf-8') as f:
             node2predict_c[year] = json.load(f)
 
     count = {}
     count_sum = {}
-    for year in range(2011, 2023):
+    for year in range(2011, 2016):
         count[year] = {}
         count_sum[year] = set()
         for category in range(8):
@@ -105,17 +93,17 @@ def analysis():
     print(count)
     # save count by pandas
     df = pd.DataFrame(count)
-    df.to_excel('data/count.xlsx')
+    df.to_excel('result/count_125.xlsx')
 
 
 def output():
     # load node2predict_c
     node2predict_c = {}
-    for year in range(2011, 2023):
-        with open('data/result/node2predict_c_{}.json'.format(year), 'r', encoding='utf-8') as f:
+    for year in range(2011, 2016):
+        with open('result/node2predict_c_{}.json'.format(year), 'r', encoding='utf-8') as f:
             node2predict_c[year] = json.load(f)
 
-    df = pd.read_csv('data/全部AB股.csv', dtype=str)
+    df = pd.read_csv('全部AB股.csv', dtype=str)
     node_list = df['股票代码'].to_list()
     node_list = ['0' * (6 - len(node)) + node for node in node_list]
 
@@ -127,7 +115,7 @@ def output():
     category_trans = {0: '高端装备', 1: '节能环保', 2: '生物', 3: '数字创意',
                       4: '新材料', 5: '新能源', 6: '新能源汽车', 7: '新一代信息技术'}
 
-    for year in range(2011, 2023):
+    for year in range(2011, 2016):
         result_list = []
         for category in range(8):
             for node, pred in node2predict_c[year][str(category)].items():
@@ -140,16 +128,15 @@ def output():
 
         # save by pandas
         df = pd.DataFrame(result_list, columns=['战新产业分类', '股票代码', '公司中文名称', '证券名称'])
-        df.to_excel('data/output/{}_result.xlsx'.format(year), index=False)
+        df.to_excel('output/{}_result.xlsx'.format(year), index=False)
 
 
 def test():
     # load node2predict_c
     node2predict_c = {}
-    with open('data/result/node2predict_c_2022.json', 'r', encoding='utf-8') as f:
-        node2predict_c[2022] = json.load(f)
-
-    df = pd.read_csv('data/全部AB股.csv', dtype=str)
+    with open('result/node2predict_c_2015.json', 'r', encoding='utf-8') as f:
+        node2predict_c = json.load(f)
+    df = pd.read_csv('全部AB股.csv', dtype=str)
     node_list = df['股票代码'].to_list()
     node_list = ['0' * (6 - len(node)) + node for node in node_list]
 
@@ -158,7 +145,7 @@ def test():
 
     result_list = []
     for category in range(8):
-        for node, pred in node2predict_c[2022][str(category)].items():
+        for node, pred in node2predict_c[str(category)].items():
             if pred == 1:
                 try:
                     result_list.append(node2node_easy_name[node])
@@ -173,13 +160,16 @@ def test():
                  '绿盟科技', '隆利科技', '信维通信', '远光软件', '浪潮软件', '长盈精密', '太极股份', '永鼎股份',
                  '卫士通', '美亚柏科']
     print(len(test_list))
+    count = 0
     for node in test_list:
         if node not in result_list:
             print(node)
+            count += 1
+    print(count)
 
 
 if __name__ == '__main__':
-    for year in range(2011, 2023):
+    for year in range(2011, 2016):
         get_inputs(year)
     analysis()
     output()
